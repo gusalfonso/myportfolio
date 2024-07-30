2c3ffda4d66b97d37b66d7b98a7d1a072be747ae
Gustavo Alfonso <alfonsgustavo@gmail.com>
2024-07-30 18:00:28 -0300

    techicons

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,32 +1,3 @@
-// import { FaInstagram, FaReact, FaCode } from "react-icons/fa";
-// import "../css/Project.css";
-// import ProjectCard from "./ProjectCard";
-// import { DATA } from "../cv/cv";
-
-// function Projects() {
-//   return (
-//     <section className="projects" id="projects">
-//       <div className="title-container">
-//         <span className="section-icon">{<FaCode />}</span>
-//         <h1 className="section-title">Projects</h1>
-//       </div>
-//       <div className="card-grid">
-//         {DATA.projects.map((project) => (
-//           <ProjectCard
-//             key={project.title}
-//             title={project.title}
-//             description={project.description}
-//             image={project.image}
-//             techIcons={project.technologies}
-//           />
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default Projects;
-
 import { FaCode } from "react-icons/fa";
 import "../css/Project.css";
 import ProjectCard from "./ProjectCard";
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,34 +1,3 @@
-// import "../css/ProjectCard.css";
-// import { ProjectCardProps } from "../types";
-// import SocialLink from "./LinkHover"; // Asegúrate de importar SocialLink
-
-// function ProjectCard({
-//   title,
-//   description,
-//   image,
-//   techIcons,
-// }: ProjectCardProps) {
-//   return (
-//     <div className={`project-card ${title}`}>
-//       <div className="card-content">
-//         <div className="text-content">
-//           <span className="project-title">{title}</span>
-//           <p className="project-description">{description}</p>
-//           <div className="icons">
-//             {techIcons.map((icon, index) => (
-//               <SocialLink key={index} title={icon.title} svg={icon.svg} />
-//             ))}
-//           </div>
-//         </div>
-//         <img className="project-image" src={image} alt={`Photo ${title}`} />
-//         <div className="image-gradient"></div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ProjectCard;
-
 import "../css/ProjectCard.css";
 import { ProjectCardProps } from "../types";
 import getIconForTech from "../utils/iconstree"; // Asegúrate de ajustar la ruta
