cf0928fd10f473593f51ff4176f9b9dcc9e515b6
Gustavo Alfonso <alfonsgustavo@gmail.com>
2024-08-28 18:28:57 +0000

    set initial lang

--- src/components/atoms/languageselector/LanguageSelector.tsx
+++ src/components/atoms/languageselector/LanguageSelector.tsx
@@ -6,12 +6,14 @@ import { Dock, DockIcon } from "../dock/Dock";
 import { useState } from "react";
 
 const LanguageSelector = () => {
-  const [lang, setLang] = useState<string>('en')
+  //const [lang, setLang] = useState<string>('en')
   const { i18n } = useTranslation();
-  //const currentLanguage = i18n.language.toUpperCase();
+  const currentLanguage = i18n.language.toUpperCase();
+  console.log(currentLanguage)
 
   const handleSelect = (code: string) => {
     i18n.changeLanguage(code.toLowerCase());
+    // setLang(code.toLowerCase())
   };
 
   return (
@@ -25,7 +27,7 @@ const LanguageSelector = () => {
             placeholder=""
             showSelectedLabel={false}
             showOptionLabel={false}
-            selected={lang}
+            selected={currentLanguage ? currentLanguage : 'en'}
           />
         </DockIcon>
       </Dock>
