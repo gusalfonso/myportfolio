53e3364d007ae8ac0fc34e31a32e88eeb2d13f77
Gustavo Alfonso <alfonsgustavo@gmail.com>
2024-08-29 06:17:51 -0300

    a ver mi solucion

--- src/components/atoms/languageselector/LanguageSelector.tsx
+++ src/components/atoms/languageselector/LanguageSelector.tsx
@@ -2,30 +2,22 @@ import { useTranslation } from "react-i18next";
 import ReactFlagsSelect from "react-flags-select";
 import "./LanguageSelector.css";
 import { Dock, DockIcon } from "../dock/Dock";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+//import { LanguageDetectorModule } from "i18next";
 
 const LanguageSelector = () => {
-  const { i18n } = useTranslation();
-
-  // Mapeo de idiomas a códigos de países.
-  const languageMap: { [key: string]: string } = {
-    en: "US",
-    es: "ES",
-    pt: "BR",
-  };
-
   const [lang, setLang] = useState<string>("US");
-
-  useEffect(() => {
-    // Ajustar el estado inicial basado en el idioma actual de i18n
-    const currentLanguageCode = i18n.language.split("-")[0]; // Obtén solo la parte "en" de "en-US", por ejemplo.
-    setLang(languageMap[currentLanguageCode] || "US");
-  }, [i18n.language]);
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language.toUpperCase();
+  console.log(currentLanguage);
+  console.log(lang);
 
   const handleSelect = (code: string) => {
-    i18n.changeLanguage(languageMap[code].toLowerCase());
+    i18n.changeLanguage(code.toLowerCase());
     setLang(code);
   };
+
+  // console.log(i18n);
 
   return (
     <div className="language-selector">
