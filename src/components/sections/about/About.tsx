ad72989ab67a5dc5ac3ce8dd13256f55736b8437
Gustavo Alfonso <alfonsgustavo@gmail.com>
2024-08-29 07:23:36 -0300

    some cleaning

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,32 +1,3 @@
-// import { BsPersonCheck } from "react-icons/bs";
-// import "../css/About.css";
-// import { DATA } from "../cv/cv";
-// import LanguageSelector from "./atoms/LanguageSelector";
-// import { useTranslation } from "react-i18next";
-
-// function About() {
-//   const { t } = useTranslation();
-//   return (
-//     <section className="aboutme" id="aboutme">
-//       <LanguageSelector />
-//       <div className="title-container">
-//         <span className="section-icon">{<BsPersonCheck />}</span>
-//         <h1 className="section-title">{t(navbar[1].label)}</h1>
-//       </div>
-//       <div className="summary">
-//         {DATA.summary
-//           .split(".  ")
-//           .map(
-//             (sentence, index) =>
-//               sentence.trim() !== "" && <p key={index}>{sentence.trim()}.</p>
-//           )}
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default About;
-
 import { BsPersonCheck } from "react-icons/bs";
 import "./About.css";
 import { useTranslation } from "react-i18next";
@@ -35,20 +6,22 @@ function About() {
   const { t } = useTranslation();
 
   return (
-    <section className="aboutme" id="aboutme">
-      <div className="title-container">
-        <span className="section-icon">{<BsPersonCheck />}</span>
-        <h1 className="section-title">{t("aboutsection")}</h1>{" "}
-      </div>
-      <div className="summary">
-        {t("summary")
-          .split(".  ")
-          .map(
-            (sentence, index) =>
-              sentence.trim() !== "" && <p key={index}>{sentence.trim()}.</p>
-          )}
-      </div>
-    </section>
+    <>
+      <section className="aboutme" id="aboutme">
+        <div className="title-container">
+          <span className="section-icon">{<BsPersonCheck />}</span>
+          <h1 className="section-title">{t("aboutsection")}</h1>{" "}
+        </div>
+        <div className="summary">
+          {t("summary")
+            .split(".  ")
+            .map(
+              (sentence, index) =>
+                sentence.trim() !== "" && <p key={index}>{sentence.trim()}.</p>
+            )}
+        </div>
+      </section>
+    </>
   );
 }
 
